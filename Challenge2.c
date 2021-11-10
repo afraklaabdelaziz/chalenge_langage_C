67a5b9a3abd48298717df540719b3141f1168cd8
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2021-11-10 14:36:25 +0100

    challenge en c

---
+++ Challenge2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float fh,c;
+	printf("donner la temperature en Fahrenheit \n");
+	scanf("%f",&fh);
+	// transformation  Fahrenheit en Celsius
+	c=(fh-32)/1.8;
+	printf("la temperature en Celsius est %f\n",c);
+	return 0; 
+	
+	
+}
---
+++ code_en_C.c
@@ -0,0 +1,19 @@
+//Challenge 1 : Affichage Informations
+#include<stdio.h>
+#include<string.h>
+int main() {
+	char nom[50],prenom[50],sexe[50],tele[50];
+	int age;
+printf("donner votre nom \n");
+scanf("%s",&nom);
+printf("donner votre prenom\n");
+scanf("%s",&prenom);
+printf("donner votre sexe\n");
+scanf("%s",&sexe);
+printf("donner votre numero de telephone\n");
+scanf("%s",&tele);
+printf("donner votre age\n");
+scanf("%d",&age);
+printf("bonjour %s %s votre sexe est %s vous avez agée %d votre numero de telephone est %s\n",nom,prenom,sexe,age,tele);
+return 0;
+}
