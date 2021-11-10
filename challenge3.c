f079122db8be2382844bcae398570a715b1b0b7b
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2021-11-10 14:55:12 +0100

    challenge

---
+++ challenge3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+ float d,M;
+ printf("donner la distance en Metre\n");
+ scanf("%f",&d);
+ M=d*1000*1.609;
+ printf("transformation de la distance Metre en Mille est %f",M);
+ return 0;
+}
