5cd53e5e7c47966809edc0c6da43494675a212c9
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2021-11-11 10:37:32 +0100

    challenge

--- challenge7.c
+++ challenge7.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main(){
 	int a,b;
-	printf("donner un entier A");
-	scanf("%2d",&a);
-		printf("donner un entier B");
+	printf("donner un entier A\n");
+	scanf("%d",&a);
+		printf("donner un entier B\n");
 	scanf("%2d",&b);
-	printf("la somme de ces nombres est :%d\n",a+b);
-	printf("la soustraction de ces nombres est : %d\n",a-b);
-	printf("le produit de ces nombres est: %d\n",a*b);
-	printf("la division de ces nombres est: %d\n",a/b);
-	printf("le modilo de ces nombres est: %d",a%b);
+	printf("la somme de ces nombres est :%i\n",a+b);
+	printf("la soustraction de ces nombres est : %i\n",a-b);
+	printf("le produit de ces nombres est: %i\n",a*b);
+	printf("la division de ces nombres est: %i\n",a/b);
+	printf("le modilo de ces nombres est: %i",a%b);
 	
 }
