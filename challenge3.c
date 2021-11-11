627de967f793f9e88c475cffde5d18e5dd262f06
afraklaabdelaziz <afraklaabdelaziz@gmail.com>
2021-11-11 11:52:04 +0100

    challenge

--- challenge3.c
+++ challenge3.c
@@ -4,7 +4,7 @@ int main(){
  float d,M;
  printf("donner la distance en Metre\n");
  scanf("%f",&d);
- M=d*1000*1.609;
+ M=d/(1000*1.609);
  printf("transformation de la distance Metre en Mille est %f",M);
  return 0;
 }
--- challenge4.c
+++ challenge4.c
@@ -4,8 +4,8 @@ int main() {
 	float d,K;
 	printf("donner la distance en MILE\n");
 	scanf("%f",&d);
-	K=(d*1000)/1.609;
-	printf("la distance en METTRE est %.3f",K);
+	K=d*(1.609*1000);
+	printf("la distance en METTRE est %.4f",K);
 	return 0;
 	
 }
